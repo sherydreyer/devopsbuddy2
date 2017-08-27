5f5d70d45a99167d6794ba2bd5b33155bcb98fd3
Sheryl Dreyer <sheryldreyer2@gmail.com>
2017-08-27 19:08:05 +0200

    initial helloworld commit

---
+++ src/main/java/com/devopsbuddy2/Devopsbuddy2Application.java
@@ -0,0 +1,12 @@
+package com.devopsbuddy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devopsbuddy2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devopsbuddy2Application.class, args);
+	}
+}
---
+++ src/test/java/com/devopsbuddy2/Devopsbuddy2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devopsbuddy2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Devopsbuddy2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
